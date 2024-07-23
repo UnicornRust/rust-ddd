eabc87454c834fcbe2add0ba13e15d38e2a0812a
ancion <ancion@github.com>
2024-07-23 17:53:21 +0800

    init project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use clean_arch::infrastructure::web::run;
+use env_logger::Env;
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info"))
+        .init();
+    run().await
+}
